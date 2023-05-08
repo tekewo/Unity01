daf4a550c872aa2fdbf2d2052ae67208013da782
tekewo <takaohiroki0620@gmail.com>
2023-05-08 19:07:28 +0900

    commit

--- unity01/Assets/Scripts/PlayerMovement.cs
+++ unity01/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float MoveSpeed = 10;
+    private Rigidbody2D myRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 force = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            force = new Vector2(MoveSpeed * -1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            force = new Vector2(MoveSpeed, 0);
+        }
+
+        myRigidbody.MovePosition(myRigidbody.position + force * Time.fixedDeltaTime);
     }
 }
