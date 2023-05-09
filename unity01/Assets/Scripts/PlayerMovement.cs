eea111a2da7e9aac46315c15ecbe2d08754a016c
nisiso <132886283+nisiso@users.noreply.github.com>
2023-05-09 22:21:04 +0900

    animetion

--- unity01/Assets/Scripts/PlayerMovement.cs
+++ unity01/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator _anim;
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
     {
         float dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y); 
-
+        _anim.SetBool("Run", dirX != 0.0f);
         if(Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, 14f);
